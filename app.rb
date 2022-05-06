ad7ca1b0aab036924f45fafe44f5c635b2734d8f
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-05-06 08:37:20 +0100

    Added route redirect. OK on Rspec, but not page

--- app.rb
+++ app.rb
@@ -6,17 +6,29 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  # Redundant initial test - paired with testing infrastructure spec
+  #Should delete, but I'm keeping it as an example for now:
   # get '/' do
   #   'Testing infrastructure working!'
   # end
+
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:session_name_1] = params[:player_1_name]
+    session[:session_name_2] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  # this is working in Rspec but not when I try to use the web page manually.
+  # the names aren't populating the /play page
+  get '/play' do
+    @name_variable_1 = session[:session_name_1]
+    @name_variable_2 = session[:session_name_2]
     erb :play
   end
 
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,6 +4,9 @@ feature 'Enter names' do
     fill_in :player_1_name, with: 'Bob'
     fill_in :player_2_name, with: 'Tina'
     click_button 'Submit'
+
+    save_and_open_page
+
     expect(page).to have_content 'Bob vs. Tina'
   end
 end
